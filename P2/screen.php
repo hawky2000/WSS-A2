92652c2d99f7ef6fb9792a67b5379dcce8beccb8
Jake Hawkins <Hawky2017@outlook.com>
2017-11-09 13:16:10 -0800

    index

---
+++ P2/screen.php
@@ -0,0 +1,32 @@
+<html>
+    <head>
+    
+        <title>Screen</title>
+        
+    </head>
+    
+    <body>
+    
+    <?php
+        
+        $width = $_POST["width"]
+            
+        $height = $_POST["height"]
+            
+        $browser = $_POST["HTTP-_USER_AGENT]
+        
+        ?>
+        <h3>
+        </php 
+            echo "Your browser is: $Browser";
+        ?>
+        </h3>
+        <h3>
+        <?php 
+            echo "you are using a screen resolution of: $width x $height";
+        ?>
+        
+        </h3>
+    
+    </body>
+</html>
